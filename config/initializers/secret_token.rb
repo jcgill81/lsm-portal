9a920b1a9c2769d25ba9dccdec1590b61ceed258
James Gill <james.gill@moovweb.com>
2013-04-24 11:21:12 -0700

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LsmPortal::Application.config.secret_token = '9c39d7767fef58762792a503ac2b5908bc86cfae5d1d8f30438cdeb092c182670cef795b4cb37d67a38ddce29558374f87598fe5cd9e15509a5c04d52982cb9c'
