df3abcde14eca541b84f54ee21e3933f7026a093
James Gill <james.gill@moovweb.com>
2013-04-26 16:18:20 -0700

    Adding sad little js password protect

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 end
+
+class AdminController < ApplicationController
+  before_filter :authenticate
+
+  def authenticate
+    authenticate_or_request_with_http_basic('Administration') do |username, password|
+      username == 'admin' && password == '201Spear'
+    end
+  end
+end
